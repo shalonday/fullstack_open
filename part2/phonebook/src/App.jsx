d8f6207a68bdf669d0781e282c1461101ba41c9a
shalonday <alondaysp@gmail.com>
2025-12-26 20:30:34 +0800

    Add exer2.6

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  function handleAdd(e) {
+    e.preventDefault();
+    setPersons((current) => [...current, { name: newName }]);
+    setNewName("");
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name:{" "}
+          <input value={newName} onChange={(e) => setNewName(e.target.value)} />
+        </div>
+        <div>
+          <button type="submit" onClick={handleAdd}>
+            add
+          </button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person, i) => (
+        <p key={i}>{person.name}</p>
+      ))}
+    </div>
+  );
+};
+
+export default App;
