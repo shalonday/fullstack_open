4498423451c3e8e7809a8cff777bdb4574b5d825
shalonday <alondaysp@gmail.com>
2025-12-26 19:39:06 +0800

    Add part2-courseinfo

---
+++ part2/courseinfo/src/components/Course.jsx
@@ -0,0 +1,31 @@
+const Header = (props) => <h2>{props.course}</h2>;
+
+const Content = (props) => (
+  <div>
+    {props.parts.map((part) => {
+      return <Part part={part} />;
+    })}
+  </div>
+);
+
+const Part = (props) => (
+  <p>
+    {props.part.name} {props.part.exercises}
+  </p>
+);
+
+const Total = (props) => <b>Total of {props.total} exercises</b>;
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total
+        total={course.parts.reduce((sum, part) => sum + part.exercises, 0)}
+      />
+    </div>
+  );
+};
+
+export default Course;
